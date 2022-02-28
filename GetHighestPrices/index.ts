f26622e289d3c4d5f5a3f68b7bf649feb5c33714
RomeoDev <45272844+Riki1200@users.noreply.github.com>
2022-02-28 12:08:43 -0400

    add new azure function for prices

---
+++ GetHighestPrices/index.ts
@@ -0,0 +1,17 @@
+﻿import { AzureFunction, Context } from '@azure/functions';
+
+const activityFunction: AzureFunction = async function (context: Context): Promise<string> {
+  //const
+  const prices = context.bindingData.input.prices;
+  if (prices) {
+    const highestPrices = prices.reduce((acc, curr) => {
+      if (curr.price > acc.price) {
+        return curr;
+      }
+      return acc;
+    });
+    return `The highest price is ${highestPrices.price}`;
+  }
+};
+
+export default activityFunction;
